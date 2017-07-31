2bb6621ca699ea809ac0326aa54bcde5bf76dc2c
Jon Jagger <jon@jaggersoft.com>
2017-07-31 15:46:31 +0530

    initial commit

---
+++ docker/red_amber_green.rb
@@ -0,0 +1,8 @@
+
+lambda { |stdout,stderr,status|
+  output = stdout + stderr
+  return :red   if /^(\d+) error/.match(output)
+  return :red   if /AssertionError/.match(output)
+  return :green if /^All tests passed/.match(output)
+  return :amber
+}
